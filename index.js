67a5f6d8cc75429b55f3375b4886a9e8668bbfef
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-02 10:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ const titleCased = () => {
     for (var i = 0; i < tutorial.length; i++) {
       tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
     }
-  return tutorials
+  return tutorials});
 }
